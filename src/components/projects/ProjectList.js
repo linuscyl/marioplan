6edd0763dfbb8f138bcf9df8bf7c98d5c8d7ba3d
Linus <linuschan@joyaether.com>
2019-09-16 18:53:15 +0800

    Tutorial 5

---
+++ src/components/projects/ProjectList.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+function ProjectList() {
+  return (
+    <div className='project-list section'>
+
+      <div className='card z-depth-0 project-summary'>
+        <span className='card-title'>Project Title</span>
+        <p>Posted by the Net Ninja</p>
+        <p className='grey-text'>3rd Septembe, 2 am</p>
+      </div>
+
+      <div className='card z-depth-0 project-summary'>
+        <span className='card-title'>Project Title</span>
+        <p>Posted by the Net Ninja</p>
+        <p className='grey-text'>3rd Septembe, 2 am</p>
+      </div>
+
+      <div className='card z-depth-0 project-summary'>
+        <span className='card-title'>Project Title</span>
+        <p>Posted by the Net Ninja</p>
+        <p className='grey-text'>3rd Septembe, 2 am</p>
+      </div>
+
+    </div>
+  )
+}
+
+export default ProjectList
